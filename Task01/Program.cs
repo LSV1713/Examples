dcf0e2264c97e45da894fbb9d8a096430c8a90f5
SVLoginova <lsvetvik@mail.ru>
2023-10-10 23:29:40 +0300

    Z1

---
+++ Task01/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int number = ReadInt("Введите трёхзначное число");
+if (number < 1000 && number >99)
+{
+    int MidleDigital = number / 10 % 10;
+    Console.WriteLine("MidleDigital = " + MidleDigital);
+    }
+    else Console.WriteLine("Число не трёхзначное");
