2de159fa12e589527ec9bece34d1b10b21e91afd
SVLoginova <lsvetvik@mail.ru>
2023-10-10 23:49:00 +0300

    Z2

---
+++ Task02/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int number = ReadInt("Введите число");
+if (number < 99)
+{
+    Console.WriteLine("третьей цифры нет");
+}
+    else 
+    {
+        while (number > 999)
+        {
+            number /= 10 ;
+        }
+
+        int TreeDigital = number % 10;
+        Console.WriteLine(TreeDigital);
+    }
