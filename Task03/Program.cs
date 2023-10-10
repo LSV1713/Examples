40f4aa1074a97829dfccbf620ae57b3b26eaa6ec
SVLoginova <lsvetvik@mail.ru>
2023-10-10 23:55:18 +0300

    Z3

---
+++ Task03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 3
+
+int ReadInt(string msg)
+{
+    Console.WriteLine(msg);
+    string numb = Console.ReadLine();
+    int number;
+    number = int.Parse(numb);
+    return number;
+}
+int number = ReadInt("Введите число");
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("да");
+}
+    else Console.WriteLine("нет");
